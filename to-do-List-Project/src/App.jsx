b186cda1679a7ebc8111b2a45c72e40b0d45d9e8
Kogha97 <poiuytrewq7991@gmail.com>
2023-11-21 12:26:47 +0000

    toggle button added

--- to-do-List-Project/src/App.jsx
+++ to-do-List-Project/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import ToDoList from './components/ToDoList'
+import ToggleButton from './components/ToggleButton'
 import './App.css'
 
 
 
 function App() {
-return 
-  
+return(
+    <>
+    <ToDoList/>
+    <br />
+    <ToggleButton/>
+    </>
+)
+
 }
 
 export default App
